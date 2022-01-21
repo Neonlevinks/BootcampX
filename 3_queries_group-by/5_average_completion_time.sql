8711558ab5b78738db6f56dac0fae2225a76c594
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-01-21 00:01:36 +0000

    Add query to find average assignment duration for currently enrolled students

---
+++ 3_queries_group-by/5_average_completion_time.sql
@@ -0,0 +1,5 @@
+SELECT name as student, AVG(duration) as average_assignment_duration FROM students
+JOIN assignment_submissions ON students.id = student_id
+WHERE end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
