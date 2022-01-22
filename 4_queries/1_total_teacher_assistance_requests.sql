c535a5fc34ecd0cfd61cace8ba63f7a4a0d2d50d
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-01-22 01:02:10 +0000

    Updated query to display column name with count as total_assistances

--- 4_queries/1_total_teacher_assistance_requests.sql
+++ 4_queries/1_total_teacher_assistance_requests.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(assistance_requests.*), teachers.name FROM assistance_requests
+SELECT COUNT(assistance_requests.*) as total_assistances, teachers.name FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 WHERE name = 'Waylon Boehm'
 GROUP BY teachers.name;
